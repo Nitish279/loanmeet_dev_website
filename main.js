510c6b73ec8eb3c869d9da4ddc2de8512e33282f
nitish67 <nitish@loanmeet.com>
2017-09-27 19:50:33 +0530

    Testimonial Changes done

--- main.js
+++ main.js
@@ -13,6 +13,46 @@ $(document).ready(function(){
     $(window).on("resize", function(){
         $(".modal:visible").each(alignModal);
     });
+
+
+     $('.loanmeet_testimonial_carousel').carousel({
+         interval: 5000
+     });
+
+     // for every slide in carousel, copy the next slide's item in the slide.
+     // Do the same for the next, next item.
+     $('.loanmeet_testimonial_carousel .item').each(function() {
+         var next = $(this).next();
+         if (!next.length) {
+             next = $(this).siblings(':first');
+         }
+         next.children(':first-child').clone().appendTo($(this));
+
+         if (next.next().length > 0) {
+             next.next().children(':first-child').clone().appendTo($(this));
+         } else {
+             $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+         }
+     });
+
+
+     // function alignVerticalCenter(){
+     //    var blockquoteHeight = $('.testimonial_item_text_block blockquote').height();
+     //    console.log("blockquoteHeight", blockquoteHeight);
+
+     //    var blockquoteContentHeight = $('.testimonial_item_text_block blockquote p').height();
+     //    console.log("blockquoteContentHeight", blockquoteContentHeight);
+
+     //    var blockquoteContentMargin =  Math.max(0, (blockquoteHeight - blockquoteContentHeight) / 2);
+     //    console.log("blockquoteContentMargin / 2", blockquoteContentMargin);
+
+
+     //    $('.testimonial_item_text_block blockquote p').css("padding-top", blockquoteContentMargin);
+
+     // }
+
+
+     // alignVerticalCenter();
        
 });
 
