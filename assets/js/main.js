979c138b0548908fe6ab58f22279724bee6e0e28
nitish67 <nitish4u029@gmail.com>
2017-12-21 04:38:20 +0530

    loanmeet all pages done with content for all the product pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
+
+
+
+ function toggleIcon(e) {
+    $(e.target)
+        .prev('.panel-heading')
+        .find(".more-less")
+        .toggleClass('glyphicon-plus glyphicon-minus');
+}
+$('.panel-group').on('hidden.bs.collapse', toggleIcon);
+$('.panel-group').on('shown.bs.collapse', toggleIcon);
 });
 
